8e4e3f7cc75365002bee7bb48d95b6cc9b41491d
Omar Kashlan <omaar_kashlan_2003@hotmail.com>
2025-04-03 20:35:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 # ============================ إعدادات البوت الأساسية ============================ #
 TOKEN = "7715192868:AAF5b5I0mfWBIuVc34AA6U6sEBt2Sb0PC6M"
-API_URL = "https://web1.shinemonitor.com/public/?sign=55ce44ea7e9677098ecc72bdb2edd074635d0a85&salt=1743065645177&token=ddf8b8429341331bc65ab85087d38e2ba1d1d15ac87a0e99b909180d3b8e3249&action=queryDeviceParsEs&source=1&devcode=2451&pn=W0040157841922&devaddr=1&sn=96322407504037&i18n=en_US"
+API_URL = "https://web1.shinemonitor.com/public/?sign=e12088382a5d2116290b90404a8f6848fbe13063&salt=1743701724736&token=1eb0086f32762b9328fbf0d9db78be937af5f1b3f6c84e6580539f538e14f8e0&action=queryDeviceParsEs&source=1&devcode=2451&pn=W0040157841922&devaddr=1&sn=96322407504037&i18n=en_US"
 
 # ============================ إعدادات المراقبة ============================ #
 BATTERY_CHANGE_THRESHOLD = 3
