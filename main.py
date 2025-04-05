975a1a91dd06a32acc598515dbbdefdca5bac400
Omar Kashlan <omaar_kashlan_2003@hotmail.com>
2025-04-05 15:44:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ import threading
 import time
 
 # ============================== CONFIGURATION ============================== #
-# Bot token
-TOKEN = "7715192868:AAFAupEHmj3SjNJ-AFUvKXoVyvchr3w5U9c"  
-# API endpoint
-API_URL = "https://web1.shinemonitor.com/public/?sign=e12088382a5d2116290b90404a8f6848fbe13063&salt=1743701724736&token=1eb0086f32762b9328fbf0d9db78be937af5f1b3f6c84e6580539f538e14f8e0&action=queryDeviceParsEs&source=1&devcode=2451&pn=W0040157841922&devaddr=1&sn=96322407504037&i18n=en_US"
-
+try:
+    from config import TELEGRAM_TOKEN, API_URL
+except ImportError:
+    # استخدام متغيرات بيئية إذا لم يتوفر ملف config.py (للاستضافة)
+    import os
+    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+    API_URL = os.environ.get("API_URL")
 # Thresholds
 BATTERY_CHANGE_THRESHOLD = 3   # Battery change percentage that triggers alert
 FRIDGE_ACTIVATION_THRESHOLD = 60  # Battery percentage needed for fridge
