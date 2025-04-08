538278d0ea1370d7930a17687177aa047763bd00
Omar Kashlan <omaar_kashlan_2003@hotmail.com>
2025-04-08 10:42:19 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# config.py
-TELEGRAM_TOKEN = "7715192868:AAFTp1KTrJo_f4DiKxj1q08VokbGbB1i3w0"  
-API_URL = "https://web1.shinemonitor.com/public/?sign=38dd69b08bad8524f60c49afafb42d1a1bac5e7d&salt=1744029612651&token=2fffc25cb3c2e998c2f2623a173e4aef930bdf4afe93120d1c2c9b953b274d5e&action=queryDeviceParsEs&source=1&devcode=2451&pn=W0040157841922&devaddr=1&sn=96322407504037&i18n=en_US"
